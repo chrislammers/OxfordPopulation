37abfb4d175ea9ae23e3244244502df57ae85f2a
chrislammers <chris.lammers00@gmail.com>
2024-10-30 15:51:59 +0000

    Finished file.

---
+++ getMedianAge.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 14:21:23 2024
+
+@author: chris
+"""
+
+
+import pandas as pd
+
+# this uses UK census data for ages of people in Oxford.
+df = pd.read_csv("oxfordAges.csv", usecols=[2,4])
+df = df.rename(columns={"Age (101 categories) Code":"Age", "Observation":"Number"})
+
+# print(df)
+
+# Add up all the people.
+totalPop = df["Number"].agg(sum)
+
+halfPop = totalPop/2
+# print(halfPop)
+
+# Count each number until you get halfway
+for age in df.values:
+    # print(age)
+    halfPop-=age[1]
+    if halfPop < 0:
+        print("The median age is", age[0])
+        break
